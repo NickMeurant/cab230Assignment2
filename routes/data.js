f6545f58cdd8f80c904bbd31a0f74d53c1391fc6
NickMeurant <n10485571@qut.edu.au>
2022-05-21 16:07:18 +1000

    volcanoes done

--- routes/data.js
+++ routes/data.js
@@ -19,22 +19,23 @@ router.get('/countries', (req, res, next) => {
 });
 
 router.get('/volcanoes', (req, res, next) => {
+  'http://sefdb02.qut.edu.au:3002/volcanoes?country=japan&populatedWithin=5km'
+
   let country = req.query.country;
   let populatedWithin = req.query.populatedWithin;
-
-  console.log(validDistance(populatedWithin));
+  
   if(populatedWithin){
     if(!validDistance(populatedWithin)){
-      res.json({error:"true", message: `Invalid value for populatedWithin: ${populatedWithin}. Only: 5km,10km,30km,100km are permitted.`});
+      res.status(400).json({error:"true", message: `Invalid value for populatedWithin: ${populatedWithin}. Only: 5km,10km,30km,100km are permitted.`});
       return;
     }
   }
 
   if (country) {
     if (populatedWithin) {
-      req.db.from('data').select('*').where("country", "=", country).where("population_" + populatedWithin, ">", "0")
+      req.db.from('data').select('id','name','country','region','subregion').where("country", "=", country).where("population_" + populatedWithin, ">", "0")
         .then((rows) => {
-          res.send(rows);
+          res.status(200).send(rows);
         })
         .catch((err) => {
           console.log(err);
@@ -42,9 +43,9 @@ router.get('/volcanoes', (req, res, next) => {
         })
     }
     else {
-      req.db.from('data').select('*').where("country", "=", country)
+      req.db.from('data').select('id','name','country','region','subregion').where("country", "=", country)
         .then((rows) => {
-          res.send(rows);
+          res.status.send(rows);
         })
         .catch((err) => {
           console.log(err);
