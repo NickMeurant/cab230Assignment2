d70d2522aa1c762912c99f3687cddcc28f328b78
NickMeurant <n10485571@qut.edu.au>
2022-05-21 22:24:41 +1000

    swagger

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = ("./docs/swaggerpet.json");
 
 const options = require('./knexfile.js');
 const knex = require('knex')(options);
@@ -22,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.use((req, res, next) => {
   req.db = knex
