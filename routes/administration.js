d2919a30e49e31e67ef2314a0c0b5af42036e1e9
NickMeurant <52279315+NickMeurant@users.noreply.github.com>
2022-08-05 13:46:32 +1000

    Update administration.js

--- routes/administration.js
+++ routes/administration.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/me', (req, res, next) => {
-  res.json({name: "Nicholas Meurant", student_number: "n10485571"});
+  res.json({name: "STUDENT NAME", student_number: "STUDENT NUMBER"});
 });
 
 module.exports = router;
